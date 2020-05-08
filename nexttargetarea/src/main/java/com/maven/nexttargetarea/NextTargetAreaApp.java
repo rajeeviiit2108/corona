06321267c7281bd4730fb0e08fefee9f4b76263c
Shagun Tyagi <Shagun Tyagi@DESKTOP-3GFNH20>
2020-05-08 18:17:40 +0530

    This is first commit from Shagun.
    Signed-off-by: Shagun Tyagi <Shagun Tyagi@DESKTOP-3GFNH20>

--- nexttargetarea/src/main/java/com/maven/nexttargetarea/NextTargetAreaApp.java
+++ nexttargetarea/src/main/java/com/maven/nexttargetarea/NextTargetAreaApp.java
@@ -9,5 +9,7 @@ public class NextTargetAreaApp
     public static void main( String[] args )
     {
         System.out.println( "how are you" );
+        System.out.println( "I am fine" );
+    
     }
 }
