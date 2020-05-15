4d179428a0b1a9133ee882e0a009dc2cebf6fb68
rajeeviiit2108 <rajeev.iiit@gmail.com>
2020-05-15 23:46:25 +0530

    Introduction of Pojo classes

---
+++ nexttargetarea/src/main/java/corona/nexttargetarea/csvpojo/TravellerDataPojo.java
@@ -0,0 +1,9 @@
+package corona.nexttargetarea.csvpojo;
+
+import java.io.Serializable;
+
+public class TravellerDataPojo implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+}
