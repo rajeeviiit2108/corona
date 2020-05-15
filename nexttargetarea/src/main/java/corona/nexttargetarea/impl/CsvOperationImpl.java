c1ee3657f49516a25956d2e9d088401979f44190
rajeeviiit2108 <rajeev.iiit@gmail.com>
2020-05-15 23:43:09 +0530

    Introduction of CSV interafce

---
+++ nexttargetarea/src/main/java/corona/nexttargetarea/impl/CsvOperationImpl.java
@@ -0,0 +1,19 @@
+package corona.nexttargetarea.impl;
+
+import corona.nexttargetarea.interfaces.CsvOperation;
+
+public class CsvOperationImpl implements CsvOperation{
+
+	@Override
+	public void readCsvFile() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pushDataToStaggingTable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ nexttargetarea/src/main/java/corona/nexttargetarea/interfaces/CsvOperation.java
@@ -0,0 +1,7 @@
+package corona.nexttargetarea.interfaces;
+
+public interface CsvOperation {
+
+	public void readCsvFile();
+	public void pushDataToStaggingTable();
+}
